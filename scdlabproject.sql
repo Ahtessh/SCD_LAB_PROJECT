df438a28919b0a677ae932edf0e4aa2375181277
Ahtesham_Khan <88922630+Ahtessh@users.noreply.github.com>
2022-01-19 08:36:06 -0800

    updated
    Updated

--- scdlabproject.sql
+++ scdlabproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2022 at 07:06 PM
+-- Generation Time: Jan 19, 2022 at 05:36 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -39,7 +39,8 @@ CREATE TABLE `addbooks` (
 
 INSERT INTO `addbooks` (`B_Id`, `B_Name`, `B_Self_No`) VALUES
 ('001', 'Harry Potter', '2'),
-('002', 'L O R', '3');
+('002', 'L O R', '3'),
+('22', 'PEAKY', '4');
 
 -- --------------------------------------------------------
 
@@ -57,7 +58,47 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`Username`, `Password`) VALUES
-('Admin', '111');
+('Admin', '111'),
+('dad', '111');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue_books`
+--
+
+CREATE TABLE `issue_books` (
+  `stu_id` varchar(10) DEFAULT NULL,
+  `book_id` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `issue_books`
+--
+
+INSERT INTO `issue_books` (`stu_id`, `book_id`) VALUES
+('3', '001'),
+('3', '002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `stu_roll` varchar(10) NOT NULL,
+  `stu_name` varchar(10) DEFAULT NULL,
+  `stu_number` varchar(14) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`stu_roll`, `stu_name`, `stu_number`) VALUES
+('2', 'PEAKY', '422'),
+('3', 's3', '333');
 
 -- --------------------------------------------------------
 
@@ -99,10 +140,34 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`Username`,`Password`);
 
 --
+-- Indexes for table `issue_books`
+--
+ALTER TABLE `issue_books`
+  ADD KEY `book_FK` (`book_id`),
+  ADD KEY `stu_FK` (`stu_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`stu_roll`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`Username`,`Password`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `issue_books`
+--
+ALTER TABLE `issue_books`
+  ADD CONSTRAINT `book_FK` FOREIGN KEY (`book_id`) REFERENCES `addbooks` (`B_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stu_FK` FOREIGN KEY (`stu_id`) REFERENCES `students` (`stu_roll`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
